53266853a19dae207b399a380bbda41a5df05be3
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-05-03 01:20:57 -0600

    Updated docker compose file for shared storage

--- app/test.py
+++ app/test.py
@@ -14,14 +14,23 @@ connection_string = os.getenv('DATABASE_URL_PYTHON')
 conn = psycopg2.connect(connection_string)
 cur = conn.cursor()
 cur.execute('''SELECT * FROM "Games"''')
-result = cur.fetchall()
+results = cur.fetchall()
 print ('Test 1: Connect to DB')
-print (len(result))
+print (len(results))
 
-with open('games.json', 'w', encoding='utf-8') as f:
-  f.write('works')
+games = dict()
+for game in results:
+  steamId = game[1]
+  title = game[2]
+  oPrice = game[3]
+  dPrice = game[4]
+  games[steamId] = ({"Title": title, "OriginalPrice": oPrice, "DiscountPrice": dPrice})
 
-print(os.path.isfile('games.json'))
+with open('/appdata/games.json', 'w', encoding='utf-8') as f:
+  json.dump(games, f, ensure_ascii=False, indent=4)
+
+print(os.path.isfile('/appdata/games.json'))
+print(len(games))
 
 print("\nclosing\n")
 conn.close()
--- app/test2.py
+++ app/test2.py
@@ -1,21 +1,19 @@
 import os
-import psycopg2
-import psycopg2.extras
-from dotenv import load_dotenv
 import json
 from tqdm import tqdm
 
 print("Test2: read games File")
 
 # Read data to insert from file
-if (os.path.isfile('games.json')):
-  file = open('games.json', encoding="utf-8")
+if (os.path.isfile('/appdata/games.json')):
+  file = open('/appdata/games.json', encoding="utf-8")
   data = json.load(file)
   
   # Insert entry into games table if it does not exist. 
   for e in tqdm(data, desc="Inserting Records..."):
-    print(e, data[e]['Title'], data[e]['OriginalPrice'], data[e]['DiscountPrice'])
+    pass
   
+  print(len(data))
 else:
   print('failed to find games.json')
 print("\nclosing\n")
